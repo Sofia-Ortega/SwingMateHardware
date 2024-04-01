eb3c9a54c28d5c8f6360c68d8d8ac28bc13cbb97
Sofia Ortega <s.ortega@tamu.edu>
2024-03-31 20:38:02 -0500

    organized files + cleaned up code

--- client/readOrientation.py
+++ client/readOrientation.py
@@ -1,9 +1,21 @@
 import asyncio
 from bleak import BleakScanner, BleakClient
 from time import sleep
+import struct
 
 CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-CHIP_NAME = "MyESP32 :D"
+
+def parse_orientation_data(value):
+    # Ensure the received value is not empty and has a length of 12 bytes (3 floats * 4 bytes each)
+    if value and len(value) == 12:
+        # Use struct.unpack to parse the bytes into floats
+        x = struct.unpack('<f', value[0:4])[0]  # Assuming little-endian byte order
+        y = struct.unpack('<f', value[4:8])[0]
+        z = struct.unpack('<f', value[8:12])[0]
+        return x, y, z
+    else:
+        print("Invalid data received:", value)
+        return None
 
 
 async def run():
@@ -11,12 +23,11 @@ async def run():
     print("Found devices:")
     for d in devices:
       print("Found device:", d.name, "with address:", d.address)
-      if d.name and CHIP_NAME in d.name:
+      if d.name and "OrientationSensorBLE" in d.name:
         print("Found device:", d.name, "with address:", d.address)
         await connect_to_device(d.address)
 
 async def connect_to_device(address):
-
   async with BleakClient(address) as client:
     print("Connected to device:", address)
 
@@ -29,9 +40,10 @@ async def connect_to_device(address):
     # READS VALUE
     while True:
       value = await client.read_gatt_char(CHARACTERISTIC_UUID)
+      orientation_data = parse_orientation_data(value)
+      if orientation_data:
+        print("Orientation data (X, Y, Z):", orientation_data)
 
-      s = value.decode('ascii')
-      print("string:", s)
       sleep(0.1)
 
 loop = asyncio.get_event_loop()
