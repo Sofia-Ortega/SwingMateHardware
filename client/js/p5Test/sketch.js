91b5dbe207021315b8318441a1499fd265582c09
Sofia Ortega <s.ortega@tamu.edu>
2024-04-05 11:09:12 -0500

    checkpoint

--- client/js/p5Test/sketch.js
+++ client/js/p5Test/sketch.js
@@ -1,56 +1,66 @@
-const boxHeight = 150;
-const boxWidth = 30;
-const jointRadius = 30;
+const BOX_HEIGHT = 150;
+const BOX_WIDTH = 30;
+const BOX_DEPTH = 30;
+const JOINT_RADIUS = 30;
 
 let rotationX = 0;
 let rotationY = 0;
 let rotationZ = 0;
 
 function setup() {
-  createCanvas(710, 400, WEBGL);
-  angleMode(DEGREES);
+    createCanvas(700, 700, WEBGL);
+    angleMode(DEGREES);
 }
 
 function draw() {
-  background(250);
-  normalMaterial();
+    background(250);
+    normalMaterial();
 
-  // Apply rotations based on keyboard input
-  if (keyIsDown(88)) {
-    // 'x' key: Rotate forward on the X-axis
-    rotationX += 1;
-  } else if (keyIsDown(83)) {
-    // 's' key: Rotate backward on the X-axis
-    rotationX -= 1;
-  }
-  if (keyIsDown(67)) {
-    // 'c' key: Rotate forward on the Y-axis
-    rotationY += 1;
-  } else if (keyIsDown(68)) {
-    //  'd' key: Rotate backward on the Y-axis
-    rotationY -= 1;
-  }
-  if (keyIsDown(90)) {
-    // 'z' key: Rotate backward on the Z-axis
-    rotationZ += 1;
-  } else if (keyIsDown(65)) {
-    // 'a' key: Rotate forward on the Z-axis
-    rotationZ -= 1;
-  }
+    // Apply rotations based on keyboard input
+    if (keyIsDown(88)) {
+        // 'x' key: Rotate forward on the X-axis
+        rotationX += 1;
+    } else if (keyIsDown(83)) {
+        // 's' key: Rotate backward on the X-axis
+        rotationX -= 1;
+    }
+    if (keyIsDown(67)) {
+        // 'c' key: Rotate forward on the Y-axis
+        rotationY += 1;
+    } else if (keyIsDown(68)) {
+        //  'd' key: Rotate backward on the Y-axis
+        rotationY -= 1;
+    }
+    if (keyIsDown(90)) {
+        // 'z' key: Rotate backward on the Z-axis
+        rotationZ += 1;
+    } else if (keyIsDown(65)) {
+        // 'a' key: Rotate forward on the Z-axis
+        rotationZ -= 1;
+    }
 
-  rotateX(rotationX);
-  rotateY(rotationY);
-  rotateZ(rotationZ);
+    push();
+    rotateX(rotationX);
+    rotateY(rotationY);
+    rotateZ(rotationZ);
 
-  // Draw the shoulder sphere
-  translate(0, (-1 * boxHeight) / 2, 0);
-  sphere(jointRadius);
-  translate(0, boxHeight / 2, 0);
+    // Draw the shoulder sphere
+    sphere(JOINT_RADIUS);
+    translate(0, BOX_HEIGHT / 2, 0);
 
-  // Draw the upper arm box
-  box(boxWidth, boxHeight, 30);
+    // Draw the upper arm box
+    box(BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH);
 
-  // Draw the elbow sphere
-  translate(0, boxHeight / 2, 0);
-  sphere(jointRadius);
+    // Draw the elbow sphere
+    translate(0, BOX_HEIGHT / 2, 0);
+    sphere(JOINT_RADIUS);
+
+    translate(0, BOX_HEIGHT / 2, 0);
+
+    rotateX(frameCount);
+    rotateY(frameCount);
+    rotateZ(frameCount);
+
+    box(BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH);
+    pop();
 }
