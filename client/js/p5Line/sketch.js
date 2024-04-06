0f894277d197f65daa7fe5ff72c22777fb0affa7
Sofia Ortega <s.ortega@tamu.edu>
2024-04-06 00:28:37 -0500

    Added Readme.md

--- client/js/p5Line/sketch.js
+++ client/js/p5Line/sketch.js
@@ -6,21 +6,41 @@ const CENTER_Y = CANVAS_HEIGHT / 2;
 
 const UPPERARM_LENGTH = 50;
 
-let v1, v2;
+let shoulder, elbow, wrist;
+let upperarm_angle = 0;
+let shoulderAngle = 45;
+let elbowAngle = 45;
 
 function setup() {
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, WEBGL);
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, WEBGL);
 
-  v1 = createVector(0, 0, 0);
-  v2 = createVector(UPPERARM_LENGTH, 0, 0);
+    camera(0, 0, 50 * sqrt(3), 0, 0, 0, 0, 1, 0);
+    perspective(PI / 3, 1, 5 * sqrt(3), 500 * sqrt(3));
+    angleMode(DEGREES);
 
-  describe("A black line connecting two spheres. The scene spins slowly.");
+    debugMode(100, 10, 0, 0, 0, 20, 0, -40, 0);
+
+    shoulder = createVector(0, 0, 0);
+    elbow = createVector(UPPERARM_LENGTH, 0, 0);
+    wrist = createVector(0, UPPERARM_LENGTH, 0);
+
+    describe("A black line connecting two spheres. The scene spins slowly.");
 }
 
 function draw() {
-  // orbitControl();
-  background(200);
+    orbitControl();
+    background(200);
 
-  // Draw a line.
-  line(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z);
+    // Draw a line.
+    push();
+    stroke(0, 0, 0);
+    strokeWeight(5);
+
+    line(shoulder.x, shoulder.y, shoulder.z, elbow.x, elbow.y, elbow.z);
+    line(elbow.x, elbow.y, elbow.z, wrist.x, wrist.y, wrist.z);
+
+    pop();
+
+    stroke(255, 0, 150);
+    strokeWeight(0.8);
 }
