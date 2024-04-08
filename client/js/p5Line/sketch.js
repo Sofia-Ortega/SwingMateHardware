aaba09daf228fdce67ceb2c176dc1a0625b7ddbb
Sofia-Ortega <s.ortega@tamu.edu>
2024-04-08 10:53:56 -0500

    added buttons to increment and decrement angles

--- client/js/p5Line/sketch.js
+++ client/js/p5Line/sketch.js
@@ -1,5 +1,5 @@
-const CANVAS_WIDTH = 700;
-const CANVAS_HEIGHT = 700;
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 400;
 
 const CENTER_X = CANVAS_WIDTH / 2;
 const CENTER_Y = CANVAS_HEIGHT / 2;
@@ -7,40 +7,70 @@ const CENTER_Y = CANVAS_HEIGHT / 2;
 const UPPERARM_LENGTH = 50;
 
 let shoulder, elbow, wrist;
-let upperarm_angle = 0;
-let shoulderAngle = 45;
-let elbowAngle = 45;
+let upperarmAngle = 0;
+let forearmAngle = 0;
+
+let angleChange = 5;
+
+let displayUpper;
+let displayFore;
 
 function setup() {
-    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, WEBGL);
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, WEBGL);
 
-    camera(0, 0, 50 * sqrt(3), 0, 0, 0, 0, 1, 0);
-    perspective(PI / 3, 1, 5 * sqrt(3), 500 * sqrt(3));
-    angleMode(DEGREES);
+  angleMode(DEGREES);
 
-    debugMode(100, 10, 0, 0, 0, 20, 0, -40, 0);
+  debugMode(100, 10, 0, 0, 0, 20, 0, -40, 0);
 
-    shoulder = createVector(0, 0, 0);
-    elbow = createVector(UPPERARM_LENGTH, 0, 0);
-    wrist = createVector(0, UPPERARM_LENGTH, 0);
+  shoulder = createVector(0, 0, 0);
+  elbow = createVector(UPPERARM_LENGTH, 0, 0);
+  wrist = createVector(0, UPPERARM_LENGTH, 0);
 
-    describe("A black line connecting two spheres. The scene spins slowly.");
+  // upper arm
+  let upperarmAnglePlusBtn = createButton("Upper +");
+  let upperarmAngleMinusBtn = createButton("Upper -");
+  upperarmAnglePlusBtn.mousePressed(() => {
+    upperarmAngle += angleChange;
+    displayUpper.html(`Upperarm: ${upperarmAngle}`);
+  });
+  upperarmAngleMinusBtn.mousePressed(() => {
+    upperarmAngle -= angleChange;
+    displayUpper.html(`Upperarm: ${upperarmAngle}`);
+  });
+
+  let forearmAnglePlusBtn = createButton("Fore +");
+  let forearmAngleMinusBtn = createButton("Fore -");
+  forearmAnglePlusBtn.position(130, 410);
+  forearmAngleMinusBtn.position(200, 410);
+  forearmAnglePlusBtn.mousePressed(() => {
+    forearmAngle += angleChange;
+    displayFore.html(`Forearm: ${forearmAngle}`);
+  });
+  forearmAngleMinusBtn.mousePressed(() => {
+    forearmAngle -= angleChange;
+    displayFore.html(`Forearm: ${forearmAngle}`);
+  });
+
+  displayUpper = createP(`Upperarm: ${upperarmAngle}`);
+  displayFore = createP(`Forearm: ${forearmAngle}`);
+
+  describe("A black line connecting two spheres. The scene spins slowly.");
 }
 
 function draw() {
-    orbitControl();
-    background(200);
+  orbitControl();
+  background(200);
 
-    // Draw a line.
-    push();
-    stroke(0, 0, 0);
-    strokeWeight(5);
+  // Draw a line.
+  push();
+  stroke(0, 0, 0);
+  strokeWeight(5);
 
-    line(shoulder.x, shoulder.y, shoulder.z, elbow.x, elbow.y, elbow.z);
-    line(elbow.x, elbow.y, elbow.z, wrist.x, wrist.y, wrist.z);
+  line(shoulder.x, shoulder.y, shoulder.z, elbow.x, elbow.y, elbow.z);
+  line(elbow.x, elbow.y, elbow.z, wrist.x, wrist.y, wrist.z);
 
-    pop();
+  pop();
 
-    stroke(255, 0, 150);
-    strokeWeight(0.8);
+  stroke(255, 0, 150);
+  strokeWeight(0.8);
 }
