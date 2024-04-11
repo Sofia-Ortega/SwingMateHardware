54eada971f6de5069d4a0be96e525b947d538286
Sofia-Ortega <s.ortega@tamu.edu>
2024-04-11 16:47:19 -0500

    fixed play slider bug

--- client/js/slider/slider.js
+++ client/js/slider/slider.js
@@ -1,118 +1,93 @@
-const GREY = "#C6C6C6";
-const GREEN = "#25daa5";
+class Slider {
+  constructor(fromSliderIdName, toSliderIdName, playSliderIdName) {
+    this.fromSlider = document.querySelector(fromSliderIdName);
+    this.toSlider = document.querySelector(toSliderIdName);
+    this.playSlider = document.querySelector(playSliderIdName);
 
-function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
-  const [from, to] = getParsed(fromInput, toInput);
-  fillSlider(fromInput, toInput, GREY, GREEN, controlSlider);
-  if (from > to) {
-    fromSlider.value = to;
-    fromInput.value = to;
-  } else {
-    fromSlider.value = from;
+    this.fromSlider.oninput = () => this.fromSliderControl();
+    this.toSlider.oninput = () => this.toSliderControl();
+    this.playSlider.oninput = () => this.playSliderControl();
+
+    this.sliderColor = "#C6C6C6"; // GREY
+    this.rangeColor = "#25daa5"; // GREEN
+
+    this.fillSlider();
+  }
+
+  fillSlider() {
+    const rangeDistance = this.toSlider.max - this.toSlider.min; // how big the slider is
+    const fromPosition = this.fromSlider.value - this.toSlider.min;
+    const toPosition = this.toSlider.value - this.toSlider.min;
+
+    // percentages
+    const startRangeColor = (fromPosition / rangeDistance) * 100;
+    const endRangeColor = (toPosition / rangeDistance) * 100; // - circleActivePercentage
+
+    // color the slider on top
+    this.toSlider.style.background = `linear-gradient(
+            to right,
+            ${this.sliderColor} 0%,
+            ${this.sliderColor} ${startRangeColor}%,
+            ${this.rangeColor} ${startRangeColor}%,
+            ${this.rangeColor} ${endRangeColor}%,
+            ${this.sliderColor} ${endRangeColor}%,
+            ${this.sliderColor} 100% 
+        )`;
+  }
+
+  fromSliderControl() {
+    this.fillSlider();
+
+    let [fromVal, playVal, toVal] = this.getValuesInt();
+
+    if (fromVal > toVal) {
+      this.fromSlider.value = toVal;
+    }
+    this.playSlider.value = fromSlider.value;
+  }
+
+  toSliderControl() {
+    this.fillSlider();
+
+    let [fromVal, playVal, toVal] = this.getValuesInt();
+
+    if (fromVal > toVal) {
+      this.toSlider.value = fromVal;
+    }
+
+    this.playSlider.value = toSlider.value;
+  }
+
+  playSliderControl() {
+    console.log("playyy");
+    let [fromVal, playVal, toVal] = this.getValuesInt();
+
+    if (playVal < fromVal) {
+      this.playSlider.value = fromVal;
+    } else if (playVal > toVal) {
+      this.playSlider.value = toVal;
+    }
+  }
+
+  getValuesInt() {
+    return [
+      this.getFromValueInt(),
+      this.getPlayValueInt(),
+      this.getToValueInt(),
+    ];
+  }
+
+  getFromValueInt() {
+    return parseInt(this.fromSlider.value, 10);
+  }
+
+  getToValueInt() {
+    return parseInt(this.toSlider.value, 10);
+  }
+
+  getPlayValueInt() {
+    return parseInt(this.playSlider.value, 10);
   }
 }
 
-function controlToInput(toSlider, fromInput, toInput, controlSlider) {
-  const [from, to] = getParsed(fromInput, toInput);
-  fillSlider(fromInput, toInput, GREY, GREEN, controlSlider);
-  setToggleAccessible(toInput);
-  if (from <= to) {
-    toSlider.value = to;
-    toInput.value = to;
-  } else {
-    toInput.value = from;
-  }
-}
-
-function controlFromSlider(fromSlider, toSlider, fromInput) {
-  const [from, to] = getParsed(fromSlider, toSlider);
-  fillSlider(fromSlider, toSlider, GREY, GREEN, toSlider);
-  if (from > to) {
-    fromSlider.value = to;
-    fromInput.value = to;
-  } else {
-    fromInput.value = from;
-  }
-}
-
-function controlPlaySlider(playSlider, fromSlider, toSlider) {
-  const [from, to] = getParsed(fromSlider, toSlider);
-  fillSlider(fromSlider, toSlider, GREY, GREEN, toSlider);
-
-  let playInt = parseInt(playSlider.value, 10);
-
-  if (playInt > to) {
-    playSlider.value = to;
-  } else if (playInt < from) {
-    playSlider.value = from;
-  } else {
-    playSlider.value = playInt;
-  }
-}
-
-function controlToSlider(fromSlider, toSlider, toInput) {
-  const [from, to] = getParsed(fromSlider, toSlider);
-  fillSlider(fromSlider, toSlider, GREY, GREEN, toSlider);
-  setToggleAccessible(toSlider);
-  if (from <= to) {
-    toSlider.value = to;
-    toInput.value = to;
-  } else {
-    toInput.value = from;
-    toSlider.value = from;
-  }
-}
-
-function getParsed(currentFrom, currentTo) {
-  const from = parseInt(currentFrom.value, 10);
-  const to = parseInt(currentTo.value, 10);
-  return [from, to];
-}
-
-/**
- *
- * @param {html_input} from
- * @param {html_input} to
- * @param {string} sliderColor
- * @param {string} rangeColor
- * @param {html_input} controlSlider
- */
-function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-  const rangeDistance = to.max - to.min;
-  const fromPosition = from.value - to.min;
-  const toPosition = to.value - to.min;
-
-  controlSlider.style.background = `linear-gradient(
-      to right,
-      ${sliderColor} 0%,
-      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
-      ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
-      ${sliderColor} 100%)`;
-}
-
-function setToggleAccessible(currentTarget) {
-  const toSlider = document.querySelector("#toSlider");
-  if (Number(currentTarget.value) <= 0) {
-    toSlider.style.zIndex = 2;
-  } else {
-    toSlider.style.zIndex = 0;
-  }
-}
-
-const fromSlider = document.querySelector("#fromSlider");
-const toSlider = document.querySelector("#toSlider");
-const playSlider = document.querySelector("#playSlider");
-const fromInput = document.querySelector("#fromInput");
-const toInput = document.querySelector("#toInput");
-
-fillSlider(fromSlider, toSlider, GREY, GREEN, toSlider);
-setToggleAccessible(toSlider);
-
-fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
-toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
-playSlider.oninput = () => controlPlaySlider(playSlider, fromSlider, toSlider);
-fromInput.oninput = () =>
-  controlFromInput(fromSlider, fromInput, toInput, toSlider);
-toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+let slider = new Slider("#fromSlider", "#toSlider", "#playSlider");
