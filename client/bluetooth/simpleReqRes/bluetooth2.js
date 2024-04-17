95b11dc6c75c5fbebf6f355b0bfb43161f888843
Sofia-Ortega <s.ortega@tamu.edu>
2024-04-17 17:15:49 -0500

    made data class

--- client/bluetooth/simpleReqRes/bluetooth2.js
+++ client/bluetooth/simpleReqRes/bluetooth2.js
@@ -3,6 +3,19 @@ const CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 
 let characteristic;
 
+class Data {
+  constructor(dataView) {
+    this.timeStamp = dataView.getFloat32(0, true);
+    this.x = dataView.getFloat32(4, true);
+    this.y = dataView.getFloat32(8, true);
+    this.z = dataView.getFloat32(12, true);
+  }
+
+  print() {
+    console.log(`${this.timeStamp}ms: [${this.x}, ${this.y}, ${this.z}]`);
+  }
+}
+
 let connectBtn = document.querySelector("#connectBtn");
 connectBtn.onclick = async () => {
   const device = await navigator.bluetooth.requestDevice({
@@ -23,14 +36,6 @@ connectBtn.onclick = async () => {
 };
 
 function onCharactieristicValueChanged(event) {
-  let value = event.target.value;
-
-  const coord = [
-    value.getFloat32(0, true),
-    value.getFloat32(4, true),
-    value.getFloat32(8, true),
-    value.getFloat32(12, true),
-  ];
-
-  console.log(coord);
+  let data = new Data(event.target.value);
+  data.print();
 }
